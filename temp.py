3e1c99b5a3b32a559a0cfffabe97c9ab002dc2e1
gyrevsk8 <gyrevsk8@gmail.com>
2019-12-03 17:03:02 +0200

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,20 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def grav_deviation():
-    d1=6
-    l=10
-    a=30
-    b=30
-    x=[0,d1*np.cos(a)]
-    y=[0,d1*np.sin(a)]
-    plt.plot(x,y)
-    d=[d1*np.cos(a),l*np.cos(a+b)]
-    f=[d1*np.sin(a),l*np.sin(a+b)]
-    plt.plot(d,f)
-    x1=[l*np.cos(a+b),l*np.cos(a+b)]
-    y1=[l*np.sin(a+b),l*np.sin(a+b)]
-    plt.plot(x1,y1,marker='o',ms=7,color='b')
+def lens(f=4,d=1,u=1):
+    #t=np.linspace(d,100,10000)
+    x=[-d,0]
+    y=[1,1]
+    if u>0:
+        j=[0,(1/(1/d+1/f))]
+        p=[1,-1]
+    else:
+        j=[0,((d*f)/(d+f))]
+        p=[1,2]
+    plt.plot(x,y,'r')
+    plt.plot(j,p,'r')
+    plt.plot([-3,4.5],[0,0],'k')
+    plt.plot([0,0],[-1.025,1.025],'c',lw=2.5)
     plt.axis('equal')
     plt.show()
-
-grav_deviation()
+lens(37,9,1)
+lens(37,9,-1)
