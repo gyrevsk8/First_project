9d31d3244ccbdfdf3bc1e1763c103f94ec475871
gyrevsk8 <gyrevsk8@gmail.com>
2019-12-03 06:17:10 +0200

    Add files via upload

--- вывввв.py
+++ вывввв.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-def circle_plotter(f=4,d=1):
+def circle_plotter(f=4,d=1,u=1):
     t=np.linspace(d,100,10000)
     x=[-d,0]
     y=[1,1]
-    j=[0,(1/(1/d+1/f))]
-    p=[1,0]
+    j=[0,(u*1/(1/d+u*1/f))]
+    p=[1,-1]
     plt.plot(x,y,'r')
     plt.plot(j,p,'r')
     plt.plot([-3,4.5],[0,0],'k')
-    plt.plot([0,0],[2,0],'k',lw=3)
+    plt.plot([0,0],[-1.025,1.025],'c',lw=2.5)
     plt.axis('equal')
     plt.show()
-circle_plotter(9,3)
+circle_plotter(9,3,1)
